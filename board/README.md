6e8f5658f08419a05bcc909f97c024f5175252f4
hhmkorea <hhmkorea@nate.com>
2024-12-12 22:29:52 +0900

    index.html

---
+++ board/README.md
@@ -0,0 +1,16 @@
+# board 
+
+### Project info 
+* Group : dotdot
+* Artifact : board
+* Type : Gradle
+* Package : com.dotdot.board 
+
+### Project setting
+* Spring Boot DevTools
+* Spring Data JPA
+* Spring Web
+* Spring Web Service
+* MariaDB Driver
+* Lombok
+* Thymeleaf 
---
+++ board/src/main/resources/static/query.sql
@@ -0,0 +1,15 @@
+-- 데이터베이스 생성
+CREATE DATABASE dotdot CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+-- 데이터베이스 사용
+use dotdot;
+-- 한글설정 확인
+show variables like 'c%';
+
+-- root에서 전체 사용자 조회
+select * from mysql.user;
+-- 사용자 추가 : 유저이름@아이피주소
+create user 'cos'@'%' identified by 'cos1234';
+-- 권한 설정 : ON DB이름.테이블명 TO 유저이름@아이피주소
+GRANT ALL PRIVILEGES ON *.* TO
+
+-- 테이블 확인
